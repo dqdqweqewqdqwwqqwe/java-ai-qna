dc5f7809cbed835c2397b4ecc40edc6e3a331ace
WinQ <93635494+dqdqweqewqdqwwqqwe@users.noreply.github.com>
2025-10-17 22:07:07 +0800

    Add files via upload

---
+++ backend/java-ai-qna/src/main/java/com/wenyou/aiqna/JavaAiQnaApplication.java
@@ -0,0 +1,13 @@
+package com.wenyou.aiqna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiQnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAiQnaApplication.class, args);
+    }
+
+}
---
+++ backend/java-ai-qna/src/test/java/org/wenyou/javaaiqna/aiqna/JavaAiQnaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wenyou.javaaiqna.aiqna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiQnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
